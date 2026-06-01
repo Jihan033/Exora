de08817590ae47046706177ba3dbb04251ae285d
aisyahh25 <aisyahnurahmad.04@gmail.com>
2026-06-02 01:38:25 +0700

    Implement Teacher Profile UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,10 +36,17 @@ android {
 }
 
 dependencies {
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
