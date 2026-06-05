86c39c2b012ce13cd7b3a7b0d98cc13b000d3ec6
Jihan Mutiara Bilqis <bilqisjihan443@gmail.com>
2026-06-05 23:38:31 +0700

    perbaikan

--- app/src/main/java/com/example/exora/admin/ClubActivity.java
+++ app/src/main/java/com/example/exora/admin/ClubActivity.java
@@ -3,8 +3,6 @@ package com.example.exora.admin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,10 +10,7 @@ import com.example.exora.R;
 
 public class ClubActivity extends AppCompatActivity {
 
-    LinearLayout btnDashboard, btnAgenda, btnClub, btnProfile, btnAllClub, btnLeadership,
-    btnSports, btnArts;
-
-    Button btnDetail;
+    LinearLayout btnDashboard, btnAgenda, btnClub, btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,57 +21,27 @@ public class ClubActivity extends AppCompatActivity {
         btnAgenda = findViewById(R.id.btnAgenda);
         btnClub = findViewById(R.id.btnClub);
         btnProfile = findViewById(R.id.btnProfile);
-        btnDetail = findViewById(R.id.btnDetail);
-        btnAllClub = findViewById(R.id.btnAllClub);
-        btnLeadership = findViewById(R.id.btnLeadership);
-        btnSports = findViewById(R.id.btnSports);
-        btnArts = findViewById(R.id.btnArts);
 
         // Dashboard
         btnDashboard.setOnClickListener(v -> {
-            Intent intent = new Intent(
-                    ClubActivity.this,
-                    DashboardActivity.class
-            );
+            Intent intent = new Intent(ClubActivity.this, DashboardActivity.class);
             startActivity(intent);
             finish();
         });
 
         // Agenda
         btnAgenda.setOnClickListener(v -> {
-
-            Intent intent =
-                    new Intent(
-                            ClubActivity.this,
-                            AgendaActivity.class
-                    );
-
+            Intent intent = new Intent(ClubActivity.this, AgendaActivity.class);
             startActivity(intent);
-
-            overridePendingTransition(
-                    R.transition.slide_in_left,
-                    R.transition.slide_out_right
-            );
-
+            overridePendingTransition(R.transition.slide_in_left, R.transition.slide_out_right);
             finish();
         });
 
         // Profile
         btnProfile.setOnClickListener(v -> {
-
-            Intent intent =
-                    new Intent(
-                            ClubActivity.this,
-                            ProfileActivity.class
-                    );
-
+            Intent intent = new Intent(ClubActivity.this, ProfileActivity.class);
             startActivity(intent);
-
-            overridePendingTransition(
-                    R.transition.slide_in_right,
-                    R.transition.slide_out_left
-            );
-
+            overridePendingTransition(R.transition.slide_in_right, R.transition.slide_out_left);
             finish();
         });
 
@@ -84,60 +49,5 @@ public class ClubActivity extends AppCompatActivity {
         btnClub.setOnClickListener(v -> {
             // stay here
         });
-
-        // VIEW DETAILS
-
-            btnDetail.setOnClickListener(v -> {
-
-                Intent intent =
-                        new Intent(
-                                ClubActivity.this,
-                                DetailActivity.class
-                        );
-
-                startActivity(intent);
-
-            });
-
-        btnAllClub.setOnClickListener(v -> {
-
-            Toast.makeText(
-                    this,
-                    "All Clubs",
-                    Toast.LENGTH_SHORT
-            ).show();
-
-        });
-
-        btnLeadership.setOnClickListener(v -> {
-
-            Toast.makeText(
-                    this,
-                    "Leadership",
-                    Toast.LENGTH_SHORT
-            ).show();
-
-        });
-
-        btnSports.setOnClickListener(v -> {
-
-            Toast.makeText(
-                    this,
-                    "Sports",
-                    Toast.LENGTH_SHORT
-            ).show();
-
-        });
-
-        btnArts.setOnClickListener(v -> {
-
-            Toast.makeText(
-                    this,
-                    "Arts",
-                    Toast.LENGTH_SHORT
-            ).show();
-
-        });
-
     }
 }
